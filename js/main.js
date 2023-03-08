3a26dd17978ce89b25d30066b5b4563113f87b69
Mahmoud Saber <engabosaber2014@gmail.com>
2023-03-08 22:21:42 +0200

    Updata Javascript code

--- js/main.js
+++ js/main.js
@@ -1,45 +1,111 @@
 // Quick view for each product
 
-const products = [
-    {
-        productName : 'Adidas Shirt',
-        productPrice : '$45',
-        productImage : 'product1',
-        addedToCart : 'false',
-    }, 
-    {
-        productName : 'Metal Bottle',
-        productPrice : '$10',
-        productImage : 'product2',
-        addedToCart : 'false',
-    },
-    {
-        productName : 'Sunglasses',
-        productPrice : '$50',
-        productImage : 'product3',
-        addedToCart : 'false',
-    },
-    {
-        productName : 'Black Hat',
-        productPrice : '$8',
-        productImage : 'product4',
-        addedToCart : 'false',
-    },
-    {
-        productName : 'BackPack',
-        productPrice : '$40',
-        productImage : 'product5',
-        addedToCart : 'false',
-    }, 
-    {
-        productName : 'Athletic shoe',
-        productPrice : '$80',
-        productImage : 'product6',
-        addedToCart : 'false',
-    },
+const cart = [];
+let products = [
+  {
+    id: "1",
+    name: "Adidas Shirt",
+    price: "$45",
+    img: "product1",
+    addedToCart: false,
+  },
+  {
+    id: "2",
+    name: "Metal Bottle",
+    price: "$10",
+    img: "product2",
+    addedToCart: false,
+  },
+  {
+    id: "3",
+    name: "Sunglasses",
+    price: "$50",
+    img: "product3",
+    addedToCart: false,
+  },
+  {
+    id: "4",
+    name: "Black Hat",
+    price: "$8",
+    img: "product4",
+    addedToCart: false,
+  },
+  {
+    id: "5",
+    name: "BackPack",
+    price: "$40",
+    img: "product5",
+    addedToCart: false,
+  },
+  {
+    id: "6",
+    name: "Athletic shoe",
+    price: "$80",
+    img: "product6",
+    addedToCart: false,
+  },
 ];
 
+// loop to create products
+// loop
+// html factory function
+// add html to document
 
+function createProducts() {
+  const productsContainer = document.getElementById("products-container");
+  productsContainer.innerHTML = "";
+  products.forEach((product) => {
+    const addToCartClass = product.addedToCart ? "bg-success" : "bg-secondary";
+    const productHtml = `
+        <div class="shop-card">
+            <img src="/img/${product.img}.jpg" alt="">
+            <h3>${product.name}</h3>
+            <span id="price">${product.price}</span>
+            <button class="button width-100 bg-primary" data-prod-id="${product.id}" onclick="viewProduct(event)">Quick View</button>
+            <button class="button width-100 ${addToCartClass}" data-prod-id="${product.id}" onclick="addToCart(event)">Add to Cart</button>
+        </div>
+        `;
+    productsContainer.innerHTML += productHtml;
+  });
+}
 
+const popup = document.getElementById("product-popup");
+const overlay = document.getElementById("overlay");
 
+function viewProduct(event) {
+  const product = products.find(
+    (product) => product.id === event.target.dataset.prodId
+  );
 
+  popup.classList.remove("d-none");
+  overlay.classList.remove("d-none");
+}
+
+function closePopup() {
+  popup.classList.add("d-none");
+  overlay.classList.add("d-none");
+}
+
+function addToCart(event) {
+  const product = products.find(
+    (product) => product.id === event.target.dataset.prodId
+  );
+  if (product.addedToCart) return;
+  products = products.map((product) => {
+    if (product.id === event.target.dataset.prodId) {
+      product.addedToCart = true;
+    }
+    return product;
+  });
+
+  cart.push(product);
+  updateCart();
+  createProducts();
+}
+
+const cartNumber = document.getElementById("cart-number");
+function updateCart() {
+  cartNumber.innerHTML = +cartNumber.innerText + 1;
+}
+
+createProducts();
