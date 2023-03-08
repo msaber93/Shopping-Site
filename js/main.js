a985dd5d076c6f73931a5830406d9d10ff5c962c
Mahmoud Saber <engabosaber2014@gmail.com>
2023-03-08 14:08:17 +0200

    trying to make javascript code

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// Quick view for each product
+
+const products = [
+    {
+        productName : 'Adidas Shirt',
+        productPrice : '$45',
+        productImage : 'product1',
+        addedToCart : 'false',
+    }, 
+    {
+        productName : 'Metal Bottle',
+        productPrice : '$10',
+        productImage : 'product2',
+        addedToCart : 'false',
+    },
+    {
+        productName : 'Sunglasses',
+        productPrice : '$50',
+        productImage : 'product3',
+        addedToCart : 'false',
+    },
+    {
+        productName : 'Black Hat',
+        productPrice : '$8',
+        productImage : 'product4',
+        addedToCart : 'false',
+    },
+    {
+        productName : 'BackPack',
+        productPrice : '$40',
+        productImage : 'product5',
+        addedToCart : 'false',
+    }, 
+    {
+        productName : 'Athletic shoe',
+        productPrice : '$80',
+        productImage : 'product6',
+        addedToCart : 'false',
+    },
+];
+
+
+
+
+
